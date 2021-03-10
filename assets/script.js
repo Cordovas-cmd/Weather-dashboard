7dfab65f4e51b6244489c678a46886b0da33d2e9
Samuel Cordova <cordovas1337@gmail.com>
2021-03-09 22:38:00 -0600

    started the JS and added variable and APi key

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+
+//API key for OpenWeather.
+var weatherKey = 'd9370cf81c44dc3900380fcc44da127d';
+
+$(document).ready(function () {
+    //Variables
+    var searchButton = $("#searchBtn");
+    var usCity = $("#cityInput");
+    var austinEl = $("#austin");
+    var chicagoEl = $("#chicago");
+    var newYorkEl = $("#newYork");
+    var orlandoEl = $("#orlando");
+    var sanFranciscoEl = $("#sanFrancisco");
+    var seattleEl = $("#seattle");
+    var denverEl = $("#denver");
+    var atlantaEl = $("#atlanta");
+
+    
